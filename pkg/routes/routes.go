1f54ff2425630a0d98148a1751a2b5539f2d630c
Jonatas Cabral <jonatas.cabral@dafiti.com.br>
2020-01-10 10:42:47 -0300

    Included basic auth

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -3,9 +3,19 @@ package routes
 import (
 	"github.com/jonatascabral/zipcodes-api/pkg/controllers"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func LoadRoutes(e *echo.Echo) {
 	e.GET("/", controllers.Ping)
-	e.POST("/import/csv", controllers.ImportCsv)
+
+	g := e.Group("/import", middleware.BasicAuth(func(username string, password string, context echo.Context) (bool, error) {
+		if username == "test" && password == "test" {
+			return true, nil
+		}
+		return false, nil
+	}))
+	{
+		g.POST("/csv", controllers.ImportCsv)
+	}
 }
